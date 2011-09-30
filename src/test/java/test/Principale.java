56311152e1a625a0e733a2937e8f00d55bd15ca2
cmzwolf@gmail.com <cmzwolf@gmail.com@39422775-8e48-bbdf-5f2d-43abd6940853>
2011-09-30 09:31:51 +0000

    Changing code of the example for using the liquid api
    git-svn-id: http://vo-param.googlecode.com/svn/trunk/model@16 39422775-8e48-bbdf-5f2d-43abd6940853

--- src/test/java/test/Principale.java
+++ src/test/java/test/Principale.java
@@ -1,6 +1,5 @@
 package test;
 
-
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 
@@ -37,27 +36,29 @@ public class Principale {
 	public static void main(String[] args) throws JAXBException,
 			FileNotFoundException {
 		// Some technical stuffs for reading the java version of the DM
-		JAXBContext jaxbContext = JAXBContext.newInstance("net.ivoa.parameter.model");
+		JAXBContext jaxbContext = JAXBContext
+				.newInstance("net.ivoa.parameter.model");
 
 		Marshaller marshaller = jaxbContext.createMarshaller();
 		marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, new Boolean(
 				true));
-		marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION, "http://www.ivoa.net/xml/Parameter/v0.1 UWS2-V1.1.xsd");
+		marshaller.setProperty(Marshaller.JAXB_SCHEMA_LOCATION,
+				"http://www.ivoa.net/xml/Parameter/v0.1 UWS2-V1.1.xsd");
 		// Creating the object factory factory
 		ObjectFactory factory = new ObjectFactory();
 
 		// Creating the root element of the schema : the service
-		UWSService service = factory.createUWSService();
-		service.setServiceId("CMZ_PROTO_01");
-		service.setServiceName("name");
+		UWSService service = factory.createUWSService()
+				.withServiceId("CMZ_PROTO_01")
+				.withServiceName("MySuperService");
 		service.setDescription("This simple service take as parameters a 3D vector speed, a mass and a time. It computes the kinetic energy and the displacement, assuming the speed is constant");
 
 		// Creating input and output parameter group
 		ParameterGroup inputsPG = factory.createParameterGroup();
 		ParameterGroup outputsPG = factory.createParameterGroup();
 
-		ParameterGroup speed = factory.createParameterGroup();
-		speed.setName("vectorSpeed");
+		ParameterGroup speed = factory.createParameterGroup().withName(
+				"vectorSpeed");
 
 		// Defining the parameters
 
@@ -116,123 +117,107 @@ public class Principale {
 		speedZ.setPrecision("0.0001");
 		speedZ.setSkossConcept("SKOSS_SPEED_COMPONENT");
 		speedZ.setUnit("m/s");
-		
-		
-                ParameterList parameterList = factory.createParameterList();
-                    
-                parameterList.getParameter().add(speedX);
-                parameterList.getParameter().add(speedY);
-                parameterList.getParameter().add(speedZ);
-                parameterList.getParameter().add(mass);
-                parameterList.getParameter().add(Kenergy);
-                parameterList.getParameter().add(time);
-                parameterList.getParameter().add(distance);
 
-                
-                service.setParameterList(parameterList);
+		ParameterList parameterList = factory.createParameterList();
+
+		parameterList.getParameter().add(speedX);
+		parameterList.getParameter().add(speedY);
+		parameterList.getParameter().add(speedZ);
+		parameterList.getParameter().add(mass);
+		parameterList.getParameter().add(Kenergy);
+		parameterList.getParameter().add(time);
+		parameterList.getParameter().add(distance);
+
+		service.setParameterList(parameterList);
 		ConstraintOnGroup speedConstraint = factory.createConstraintOnGroup();
 
 		// Creating the expression for relativistic limit on speed vector
+		ParameterReference speedxref = new ParameterReference()
+				.withParameterName(speedX.getName());
+
 		AtomicParameterExpression vx2 = factory
-				.createAtomicParameterExpression();
-		ParameterReference speedxref = new ParameterReference();
-		speedxref.setParameterName(speedX.getName());
-		vx2.setParameterRef(speedxref);
-		vx2.setPower("2");
+				.createAtomicParameterExpression().withParameterRef(speedxref)
+				.withPower("2");
+
+		ParameterReference speedyref = new ParameterReference()
+				.withParameterName(speedY.getName());
 
 		AtomicParameterExpression vy2 = factory
-				.createAtomicParameterExpression();
-                ParameterReference speedyref = new ParameterReference();
-                speedyref.setParameterName(speedY.getName());
-		vy2.setParameterRef(speedyref);
-		vy2.setPower("2");
+				.createAtomicParameterExpression().withParameterRef(speedyref)
+				.withPower("2");
+
+		ParameterReference speedzref = new ParameterReference()
+				.withParameterName(speedZ.getName());
 
 		AtomicParameterExpression vz2 = factory
-				.createAtomicParameterExpression();
-                ParameterReference speedzref = new ParameterReference();
-                speedzref.setParameterName(speedZ.getName());
-		vz2.setParameterRef(speedzref);
-		vz2.setPower("2");
+				.createAtomicParameterExpression().withParameterRef(speedzref)
+				.withPower("2");
 
-		Operation plusVZ2 = factory.createOperation();
-		plusVZ2.setOperationType("PLUS");
-		plusVZ2.setExpression(vz2);
+		Operation plusVZ2 = factory.createOperation().withOperationType("PLUS")
+				.withExpression(vz2);
 
 		vy2.setOperation(plusVZ2);
 
-		Operation plusVY2 = factory.createOperation();
-		plusVY2.setOperationType("PLUS");
-		plusVY2.setExpression(vy2);
+		Operation plusVY2 = factory.createOperation().withOperationType("PLUS")
+				.withExpression(vy2);
 
 		vx2.setOperation(plusVY2);
 
-		ParenthesisContent speedNorm = factory.createParenthesisContent();
-		speedNorm.setExpression(vx2);
-		speedNorm.setPower("0.5");
+		ParenthesisContent speedNorm = factory.createParenthesisContent()
+				.withExpression(vx2).withPower("0.5");
 
 		// Creating the criterion related to the relativistic speed constraint
-		Criterion speedCriterion = factory.createCriterion();
-		speedCriterion.setExpression(speedNorm);
+		Criterion speedCriterion = factory.createCriterion().withExpression(
+				speedNorm);
 
-		//example of constructor with all values set
+		// example of constructor with all values set
 		ValueLargerThan vlt2 = new ValueLargerThan("0", true);
-		ValueSmallerThan vst2 = factory.createValueSmallerThan();
-		vst2.setValue("299792458");
-		vst2.setReached(false);
-		ValueInRange vir2 = factory.createValueInRange();
-		vir2.setInf(vlt2);
-		vir2.setSup(vst2);
+		ValueSmallerThan vst2 = new ValueSmallerThan("299792458", false);
+
+		ValueInRange vir2 = factory.createValueInRange().withInf(vlt2)
+				.withSup(vst2);
+
 		speedCriterion.setConditionType(vir2);
 
-		Always speedCondition = factory.createAlways();
-		speedCondition.setCriterion(speedCriterion);
+		Always speedCondition = factory.createAlways().withCriterion(
+				speedCriterion);
 
 		// Adding the three components of speed to the group speed
 		speed.getParameterRef().add(speedxref);
 		speed.getParameterRef().add(speedyref);
 		speed.getParameterRef().add(speedzref);
-		
-		
-		AlwaysConditionalStatement alwaysStatement = factory.createAlwaysConditionalStatement();
-		
-		alwaysStatement.setAlways(speedCondition);
-		
-		
+
+		AlwaysConditionalStatement alwaysStatement = factory
+				.createAlwaysConditionalStatement().withAlways(speedCondition);
+
 		speedConstraint.getConditionalStatement().add(alwaysStatement);
 
 		// Adding the speed constraint to the group speed
 		speed.setConstraintOnGroup(speedConstraint);
 
-		
-		
 		// Defining constraint on time (must be positive)
+		ParameterReference timeref = new ParameterReference()
+				.withParameterName(time.getName());
+
 		AtomicParameterExpression timeExpression = factory
-				.createAtomicParameterExpression();
-		ParameterReference timeref = new ParameterReference(); 
-		timeref.setParameterName(time.getName());
-                timeExpression.setParameterRef(timeref);
+				.createAtomicParameterExpression().withParameterRef(timeref);
 
-		Criterion timeCriterion = factory.createCriterion();
-		timeCriterion.setExpression(timeExpression);
-		ValueLargerThan vltime = factory.createValueLargerThan();
-		vltime.setValue("0");
-		vltime.setReached(true);
-		timeCriterion.setConditionType(vltime);
+		ValueLargerThan vltime = new ValueLargerThan("0", true);
+		Criterion timeCriterion = factory.createCriterion()
+				.withExpression(timeExpression).withConditionType(vltime);
 
-		
 		// Defining constraint on mass (must be positive)
-		AtomicParameterExpression massExpression = factory
-				.createAtomicParameterExpression();
 		ParameterReference massRef = mkRef(mass);
-                massExpression.setParameterRef(massRef);
 
-		Criterion massCriterion = factory.createCriterion();
-		massCriterion.setExpression(massExpression);
-		//example of the "fluid" api
-		ValueLargerThan vlmass = factory.createValueLargerThan().withValue("0").withReached(false);
-		vlmass.setValue("0");
-		vlmass.setReached(true);
-		massCriterion.setConditionType(vlmass);
+		AtomicParameterExpression massExpression = factory
+				.createAtomicParameterExpression().withParameterRef(massRef);
+
+		// example of the "fluid" api
+		ValueLargerThan vlmass = factory.createValueLargerThan().withValue("0")
+				.withReached(false);
+
+		Criterion massCriterion = factory.createCriterion()
+				.withExpression(massExpression).withConditionType(vlmass);
 
 		// Workin a little bit on inputs
 		inputsPG.getParameterRef().add(massRef);
@@ -241,51 +226,48 @@ public class Principale {
 		inputsPG.getParameterGroup().add(speed);
 
 		ConstraintOnGroup inputsConstraint = factory.createConstraintOnGroup();
-		
-		LogicalConnector massAndTime = factory.createAnd();
-		massAndTime.setCriterion(timeCriterion);
+
+		LogicalConnector massAndTime = factory.createAnd().withCriterion(
+				timeCriterion);
+
 		massCriterion.setLogicalConnector(massAndTime);
-		
-		Always massAndTimePositive = factory.createAlways();
-		massAndTimePositive.setCriterion(massCriterion);
-		
-		
-		AlwaysConditionalStatement massAndTimeAlwaysStatement = factory.createAlwaysConditionalStatement();
-		massAndTimeAlwaysStatement.setAlways(massAndTimePositive);
-		
-		inputsConstraint.getConditionalStatement().add(massAndTimeAlwaysStatement);
-		
+
+		Always massAndTimePositive = factory.createAlways().withCriterion(
+				massCriterion);
+
+		AlwaysConditionalStatement massAndTimeAlwaysStatement = factory
+				.createAlwaysConditionalStatement().withAlways(
+						massAndTimePositive);
+
+		inputsConstraint.getConditionalStatement().add(
+				massAndTimeAlwaysStatement);
 
 		inputsPG.setConstraintOnGroup(inputsConstraint);
 
 		// Defining constraint on kinetic energy (must be positive)
-		AtomicParameterExpression kineticExpression = factory
-				.createAtomicParameterExpression();
 		ParameterReference KenergyRef = mkRef(Kenergy);
-        kineticExpression.setParameterRef(KenergyRef);
 
-		Criterion kineticCriterion = factory.createCriterion();
-		kineticCriterion.setExpression(kineticExpression);
-		ValueLargerThan vlkinetic = factory.createValueLargerThan();
-		vlkinetic.setValue("0");
-		vlkinetic.setReached(true);
-		kineticCriterion.setConditionType(vlkinetic);
+		AtomicParameterExpression kineticExpression = factory
+				.createAtomicParameterExpression().withParameterRef(KenergyRef);
 
-				
-		Always alwaysKinetic = factory.createAlways();
-		alwaysKinetic.setCriterion(kineticCriterion);
-		
-		AlwaysConditionalStatement alwaysKineticEnergyPositiveStatement = factory.createAlwaysConditionalStatement();
-		alwaysKineticEnergyPositiveStatement.setAlways(alwaysKinetic);
-		
-		
+		ValueLargerThan vlkinetic = new ValueLargerThan("0", true);
+
+		Criterion kineticCriterion = factory.createCriterion()
+				.withExpression(kineticExpression).withConditionType(vlkinetic);
+
+		Always alwaysKinetic = factory.createAlways().withCriterion(
+				kineticCriterion);
+
+		AlwaysConditionalStatement alwaysKineticEnergyPositiveStatement = factory
+				.createAlwaysConditionalStatement().withAlways(alwaysKinetic);
 
 		// Working on outputs
 		outputsPG.getParameterRef().add(KenergyRef);
 		outputsPG.getParameterRef().add(mkRef(distance));
 
 		ConstraintOnGroup outputsConstraint = factory.createConstraintOnGroup();
-		outputsConstraint.getConditionalStatement().add(alwaysKineticEnergyPositiveStatement);
+		outputsConstraint.getConditionalStatement().add(
+				alwaysKineticEnergyPositiveStatement);
 
 		outputsPG.setConstraintOnGroup(outputsConstraint);
 
@@ -293,14 +275,14 @@ public class Principale {
 		service.setInputs(inputsPG);
 		service.setOutputs(outputsPG);
 
-		marshaller.marshal(service, new FileOutputStream("UWS_TEST.xml"));
+		marshaller.marshal(service, new FileOutputStream("/Users/zwolf/Desktop/UWS_TEST.xml"));
 
 	}
-	
-	private static ParameterReference mkRef(SingleParameter par){
-	    ParameterReference ref = new ParameterReference();
-	    ref.setParameterName(par.getName());
-            return ref ;
+
+	private static ParameterReference mkRef(SingleParameter par) {
+		ParameterReference ref = new ParameterReference();
+		ref.setParameterName(par.getName());
+		return ref;
 	}
 
 }
