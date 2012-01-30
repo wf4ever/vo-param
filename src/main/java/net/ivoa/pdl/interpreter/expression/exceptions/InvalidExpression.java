d0fd4f4df76a0d4eb3afeb7008444330b8fb75cf
cmzwolf@gmail.com <cmzwolf@gmail.com@39422775-8e48-bbdf-5f2d-43abd6940853>
2012-01-30 10:39:01 +0000

    initiate PDL interpreter
    git-svn-id: http://vo-param.googlecode.com/svn/trunk/model@45 39422775-8e48-bbdf-5f2d-43abd6940853

---
+++ src/main/java/net/ivoa/pdl/interpreter/expression/exceptions/InvalidExpression.java
@@ -0,0 +1,13 @@
+package net.ivoa.pdl.interpreter.expression.exceptions;
+
+public class InvalidExpression extends Exception {
+	private static final long serialVersionUID = -5315405384001086653L;
+
+	public InvalidExpression(String message) {
+        super(message);
+    }
+
+    public InvalidExpression(String message, Throwable cuase) {
+        super(message, cuase);
+    }
+}
