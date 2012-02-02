213817a7fc1fc80f43fccaf9ea26ed47c40d056f
cmzwolf@gmail.com <cmzwolf@gmail.com@39422775-8e48-bbdf-5f2d-43abd6940853>
2012-02-02 13:40:34 +0000

    refactoring packages
    git-svn-id: http://vo-param.googlecode.com/svn/trunk/model@57 39422775-8e48-bbdf-5f2d-43abd6940853

---
+++ src/main/java/exeptions/InvalidCondition.java
@@ -0,0 +1,13 @@
+package exeptions;
+
+public class InvalidCondition extends Exception {
+	private static final long serialVersionUID = -5315405384001086653L;
+
+	public InvalidCondition(String message) {
+        super(message);
+    }
+
+    public InvalidCondition(String message, Throwable cuase) {
+        super(message, cuase);
+    }
+}
---
+++ src/main/java/exeptions/InvalidCriterion.java
@@ -0,0 +1,13 @@
+package exeptions;
+
+public class InvalidCriterion extends Exception {
+	private static final long serialVersionUID = -5315405384001086653L;
+
+	public InvalidCriterion(String message) {
+        super(message);
+    }
+
+    public InvalidCriterion(String message, Throwable cuase) {
+        super(message, cuase);
+    }
+}
