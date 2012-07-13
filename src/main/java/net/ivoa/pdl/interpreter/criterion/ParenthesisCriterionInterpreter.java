f9764322792cec18a3e388cd59aaaea7df17f1ec
cmzwolf@gmail.com <cmzwolf@gmail.com@39422775-8e48-bbdf-5f2d-43abd6940853>
2012-07-13 08:06:44 +0000

    Correction of a (big?) mistake in interpreting parenthesis criterion.
    git-svn-id: http://vo-param.googlecode.com/svn/trunk/model@131 39422775-8e48-bbdf-5f2d-43abd6940853

--- src/main/java/net/ivoa/pdl/interpreter/criterion/ParenthesisCriterionInterpreter.java
+++ src/main/java/net/ivoa/pdl/interpreter/criterion/ParenthesisCriterionInterpreter.java
@@ -1,5 +1,6 @@
 package net.ivoa.pdl.interpreter.criterion;
 
+import net.ivoa.parameter.model.AbstractCriterion;
 import net.ivoa.parameter.model.Criterion;
 import net.ivoa.parameter.model.ParenthesisCriterion;
 import exeptions.InvalidCondition;
@@ -7,30 +8,34 @@ import exeptions.InvalidCriterion;
 import exeptions.InvalidExpression;
 import exeptions.InvalidParameterException;
 
-public class ParenthesisCriterionInterpreter extends AbstractCriterionInterpreter {
-	
+public class ParenthesisCriterionInterpreter extends
+		AbstractCriterionInterpreter {
+
 	public ParenthesisCriterionInterpreter(ParenthesisCriterion criterion) {
 		super();
 		this.criterion = criterion;
 	}
 
-	private ParenthesisCriterion criterion;	
-	
+	private ParenthesisCriterion criterion;
+
 	@Override
 	public boolean isCriterionSatisfied() throws InvalidExpression,
 			InvalidParameterException, InvalidCondition, InvalidCriterion {
-		
-		Criterion containedCriterion = new Criterion();
-		containedCriterion.withExpression(this.criterion.getExpression());
-		containedCriterion.withConditionType(this.criterion.getConditionType());
-		containedCriterion.withLogicalConnector(this.criterion.getLogicalConnector());
-		
-		boolean toReturn = new CriterionInterpreter(containedCriterion).isCriterionSatisfied();
-		
+
+		AbstractCriterion containedCriterion = new Criterion(
+				this.criterion.getExpression(),
+				this.criterion.getConditionType(),
+				this.criterion.getLogicalConnector());
+
+		boolean toReturn = CriterionInterpreterFactory.getInstance()
+				.buildCriterrionInterpreter(containedCriterion)
+				.isCriterionSatisfied();
+
 		// If the logical connector is not null
 		if (null != this.criterion.getLogicalConnector()) {
 			toReturn = new LogicalConnectorInterpreter(
-					this.criterion.getExternalLogicalConnector()).interpret(toReturn);
+					this.criterion.getExternalLogicalConnector())
+					.interpret(toReturn);
 		}
 		return toReturn;
 	}
