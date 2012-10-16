b16d3784bfa45923f799103c0b500fd6eb14d488
cmzwolf@gmail.com <cmzwolf@gmail.com@39422775-8e48-bbdf-5f2d-43abd6940853>
2012-10-16 10:43:55 +0000

    Creation of this class for getting the semantic descriptions of parameters starting from their Skos concept
    git-svn-id: http://vo-param.googlecode.com/svn/trunk/model@156 39422775-8e48-bbdf-5f2d-43abd6940853

---
+++ src/main/java/net/ivoa/pdl/interpreter/utilities/SkossConverter.java
@@ -0,0 +1,76 @@
+package net.ivoa.pdl.interpreter.utilities;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import net.ivoa.parameter.model.SingleParameter;
+
+
+public class SkossConverter {
+
+	private static final SkossConverter instance = new SkossConverter();
+
+	private SkossConverter() {
+		super();
+		initialiseSkossMap();
+	}
+
+	public static SkossConverter getInstance() {
+		return instance;
+	}
+	
+	private Map<String, String> urlSkossDescription;
+	
+	private void initialiseSkossMap(){
+		urlSkossDescription = new HashMap<String, String>();
+		List<SingleParameter> parameterList = Utilities.getInstance().getService().getParameters().getParameter();
+		for(SingleParameter currentParam : parameterList){
+			urlSkossDescription.put(currentParam.getSkossConcept(), getDescriptionBySkos(currentParam.getSkossConcept()));
+		}
+	}
+	
+	
+	public String getSkosDescriptionBySkosURI(String uri){
+		return urlSkossDescription.get(uri);
+	}
+	
+
+	private String getDescriptionBySkos(String skosUrl) {
+		String toReturn = "";
+		try {
+			String requestResult;
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(new URL(skosUrl).openConnection()
+							.getInputStream()));
+			StringBuilder sb = new StringBuilder();
+			String line = null;
+			while ((line = bufferedReader.readLine()) != null) {
+				sb.append(line);
+				sb.append("\n");
+			}
+			bufferedReader.close();
+			requestResult = sb.toString();
+			int begin = requestResult.indexOf("<skos:definition xml:lang=\"en\">")+31;
+			int end = requestResult.indexOf("</skos:definition>");
+			toReturn = requestResult.substring(begin, end);
+			String [] temp = toReturn.split(" ");
+			toReturn = "<html>";
+			for(int i =0;i < temp.length; i++){
+				toReturn = toReturn + " " + temp[i];
+				if ((i+1)%4==0){
+					toReturn = toReturn+ "<br>";
+				}
+			}
+			toReturn = toReturn + "</html>";
+		} catch (Exception e) {
+			//e.printStackTrace();
+			System.out.println("error retriving "+skosUrl);
+		}
+		return toReturn;
+	}
+
+}
