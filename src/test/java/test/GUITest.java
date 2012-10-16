f619a3771fe47f0c0f5116e9338a7337189ea662
cmzwolf@gmail.com <cmzwolf@gmail.com@39422775-8e48-bbdf-5f2d-43abd6940853>
2012-10-16 10:45:18 +0000

    Modification for initializing the semantic description of parameters
    git-svn-id: http://vo-param.googlecode.com/svn/trunk/model@157 39422775-8e48-bbdf-5f2d-43abd6940853

--- src/test/java/test/GUITest.java
+++ src/test/java/test/GUITest.java
@@ -2,12 +2,14 @@ package test;
 
 import java.io.File;
 
+import javax.swing.ToolTipManager;
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 
 import net.ivoa.gui.IntelligentGUI;
 import net.ivoa.parameter.model.Service;
+import net.ivoa.pdl.interpreter.utilities.SkossConverter;
 import net.ivoa.pdl.interpreter.utilities.UserMapper;
 import net.ivoa.pdl.interpreter.utilities.Utilities;
 
@@ -22,8 +24,13 @@ public class GUITest {
 		// CRUCIAL!! Initialize the utilities
 		Utilities.getInstance().setService(service);
 		Utilities.getInstance().setMapper(new UserMapper());
-
+		
+		ToolTipManager.sharedInstance().setDismissDelay(60000);
 		IntelligentGUI gui = new IntelligentGUI(service);
+		
+		//Initialise the description of parameters from their Skos concepts
+		SkossConverter.getInstance();
+		
 		gui.createAndShowGUI();
 	}
 
