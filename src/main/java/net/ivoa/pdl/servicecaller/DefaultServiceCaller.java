59a36910e9c34ced3321e2b69c0f21420dbcc908
cmzwolf@gmail.com <cmzwolf@gmail.com@39422775-8e48-bbdf-5f2d-43abd6940853>
2013-02-06 14:25:31 +0000

    including character encoding for string parameters containing special characters (as / @ and similiar)
    git-svn-id: http://vo-param.googlecode.com/svn/trunk/model@189 39422775-8e48-bbdf-5f2d-43abd6940853

--- src/main/java/net/ivoa/pdl/servicecaller/DefaultServiceCaller.java
+++ src/main/java/net/ivoa/pdl/servicecaller/DefaultServiceCaller.java
@@ -4,7 +4,9 @@ import java.util.List;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
+import java.net.URLEncoder;
 
+import net.ivoa.parameter.model.ParameterType;
 import net.ivoa.parameter.model.SingleParameter;
 import net.ivoa.pdl.interpreter.utilities.Utilities;
 
@@ -13,37 +15,39 @@ public class DefaultServiceCaller implements IserviceCaller {
 	public String callService() {
 		String serviceUrl = Utilities.getInstance().getService().getServiceId()
 				+ "?";
-		
-		
+
 		List<SingleParameter> paramList = Utilities.getInstance().getService()
 				.getParameters().getParameter();
-		
+
 		for (int i = 0; i < paramList.size(); i++) {
-			try{
-			
-			SingleParameter p = paramList.get(i);
-			String character = "";
-			if (i > 0) {
-				character = "&";
-			}
-			serviceUrl = serviceUrl
-					+ character
-					+ p.getName()
-					+ "="
-					+ Utilities.getInstance()
-							.getuserProvidedValuesForParameter(p).get(0)
-							.getValue();
-			}catch (Exception e) {
+			try {
+
+				SingleParameter p = paramList.get(i);
+				String character = "";
+				if (i > 0) {
+					character = "&";
+				}
+
+				String paramName = p.getName();
+				String paramValue = Utilities.getInstance()
+						.getuserProvidedValuesForParameter(p).get(0).getValue();
+				if (p.getParameterType().equals(ParameterType.STRING)) {
+					paramValue = URLEncoder.encode(paramValue, "UTF-8");
+				}
+
+				serviceUrl = serviceUrl + character + paramName + "="
+						+ paramValue;
+
+			} catch (Exception e) {
 				// TODO: do nothing
 			}
 		}
 		System.out.println(serviceUrl);
 		try {
-			
 			BufferedReader bufferedReader = new BufferedReader(
 					new InputStreamReader(new URL(serviceUrl).openConnection()
 							.getInputStream()));
-			
+
 			StringBuilder sb = new StringBuilder();
 			String line = null;
 			while ((line = bufferedReader.readLine()) != null) {
